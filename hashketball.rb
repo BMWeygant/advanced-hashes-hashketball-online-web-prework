c4bd0bf23524701e0b76236ea47406dc1ec8381d
BRANDON M WEYGANT <bobbydigital108@gmail.com>
2019-02-19 16:20:54 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -200,8 +200,7 @@ end
 def big_shoe_rebounds
 game_hash.each do |location, team_data|
   #binding.pry
-game_hash[location][:players][:shoe].sort_by{|player, shoe_size| shoe_size}
-return game_hash[location][:players][:shoe].last(:rebounds)
+
 #binding.pry
 end
 end
